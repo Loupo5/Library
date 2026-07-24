a41c8665381a2ed32f3673854a03e50365949b6b
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-24 11:08:11 +0200

    Make the table display a new row with every td

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const tbody = document.querySelector("tbody")
 
 
 function Book(title, author, pages, read) {
+    if (!new.target)  {
+        throw Error("Use the 'new' operator")
+    }
     this.title = title
     this.author = author 
     this.pages = pages
@@ -12,13 +15,24 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID()
 }
 
-function addBookToLibrary(Book) {
-    myLibrary.push(Book)
-    const tableRow = document.createElement("tr")
-    for (key in Book) {
-        tableRow.textContent = key;
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+    for (let book of myLibrary) {
+        const tableRow = document.createElement("tr")
+        const tableData = document.createElement("td")
+        for (let i=1; i<myLibrary.length + 1; i++) {
+            tableData.textContent = `Book ${i}`
+            tableRow.appendChild(tableData)
+        }
+        for (let key in book) {
+            if (key === "id") continue
+            const tableData = document.createElement("td")
+            tableData.textContent = book[key]
+            tableRow.appendChild(tableData)
+        }
+        tbody.appendChild(tableRow)
     }
-    tableRow.appendChild(tbody)
+    
 }
 let HarryPooper = new Book("herypoter", "jk rwling", 234, "no")
 addBookToLibrary(HarryPooper)
