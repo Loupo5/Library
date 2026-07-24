91ceaffc35e63c7a3ac451524e74615ef070ad54
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-24 16:03:49 +0200

    Make the addBook func show number of the book

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let HarryPooper = new Book("herypoter", "jk rwling", 234, "no")
 
 add_book_btn.addEventListener("click", (e) => {
     e.preventDefault()
-    let newBook = new Book(book_title.value, book_author.value, book_pages.value, book_read.value)
+    const newBook = new Book(book_title.value, book_author.value, book_pages.value, book_read.value)
     addBookToLibrary(newBook)
     book_title.value = ""
     book_author.value = ""
@@ -32,21 +32,16 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
-    for (let book of myLibrary) {
-        const tableRow = document.createElement("tr")
+    const tableRow = document.createElement("tr")
+    const tableData = document.createElement("td")
+    tableData.textContent = `Book ${myLibrary.length}`
+    tableRow.appendChild(tableData)
+    for (let key in book) {
+        if (book[key] === book.id) continue /*Skips showing the id key*/
         const tableData = document.createElement("td")
-        for (let i=1; i<myLibrary.length + 1; i++) {
-            tableData.textContent = `Book ${i}`
-            tableRow.appendChild(tableData)
-        }
-        for (let key in book) {
-            if (book[key] === book.id) continue /*Skips showing the id key*/
-            const tableData = document.createElement("td")
-            tableData.textContent = book[key]
-            tableRow.appendChild(tableData)
-        }
-        tbody.appendChild(tableRow)
+        tableData.textContent = book[key]
+        tableRow.appendChild(tableData)
     }
-    
+    tbody.appendChild(tableRow) 
 }
 
